9d66f4b105ba17c2a4b7aef321f254773649db25
AyoOluwa-Israel <izzyfresher@gmail.com>
2021-04-02 02:00:04 +0100

    1sr commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import Head from 'next/head'
 import Layout from '../components/Layout/Layout'
+import SearchInput from '../components/SearchInput/SearchInput';
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ countries }) {
+  console.log(countries);
   return (
-   <Layout>main</Layout>
+   <Layout>
+     <div className={styles.counts}>Found {countries.length} countries</div>
+
+     <SearchInput placeholder="Filter By Name, Religion or SubRegion" />
+   </Layout>
   )
+}
+
+
+export const getStaticProps = async () => {
+  const res = await fetch ("https://restcountries.eu/rest/v2/all")
+  const countries = await res.json();
+
+  return{
+    props: {
+      countries,
+    },
+  }
 }
