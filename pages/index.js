620e7b61dc353c03edcb04eff2b4a154273c65bd
AyoOluwa-Israel <izzyfresher@gmail.com>
2021-04-03 12:51:34 +0100

    2nd commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Head from 'next/head'
+import CountriesTable from '../components/CountriesTable/CountriesTable';
 import Layout from '../components/Layout/Layout'
 import SearchInput from '../components/SearchInput/SearchInput';
 import styles from '../styles/Home.module.css'
 
 export default function Home({ countries }) {
-  console.log(countries);
+  // console.log(countries);
   return (
    <Layout>
      <div className={styles.counts}>Found {countries.length} countries</div>
 
      <SearchInput placeholder="Filter By Name, Religion or SubRegion" />
+     <CountriesTable countries ={countries} />
    </Layout>
   )
 }
